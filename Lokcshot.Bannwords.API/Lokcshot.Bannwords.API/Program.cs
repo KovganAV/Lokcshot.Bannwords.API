054e80c5ba44a74e67d1dc645bbc71920b289aac
Marsisanya <kovgana25@gmail.com>
2024-09-23 12:58:07 +0300

    Add Program from discord

---
+++ Lokcshot.Bannwords.API/Lokcshot.Bannwords.API/Program.cs
@@ -0,0 +1,80 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Discord.AppSettings.Api.Core.Interfaces;
+using Discord.AppSettings.Api.Core.Services;
+using Discord.AppSettings.Api.Data.Repositories;
+using Discord.AppSettings.Api.Data.DataBaseContext;
+using Discord.AppSettings.Api.Core.Service;
+using Discord.AppSettings.Api.Data.Entities;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "ClientWebApi",
+        policy =>
+        {
+            policy.WithOrigins("http://192.168.17.57:7100", "http://localhost:5290", "http://192.168.17.57:3001")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+        });
+});
+
+builder.Services.AddScoped<ICountryBannedWordsService, CountryBannedWordsService>();
+builder.Services.AddScoped<CountryBannedWordsRepository>();
+builder.Services.AddScoped<ICountryService, CountryServise>();
+builder.Services.AddScoped<BaseRepository<CountryEntity>>();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
+app.UseCors("ClientWebApi");
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+try
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<ApplicationDbContext>();
+
+    if ((await context.Database.GetPendingMigrationsAsync()).Any())
+    {
+        context.Database.Migrate();
+    }
+    else
+    {
+        context.Database.EnsureCreated();
+    }
+
+}
+catch (Exception ex)
+{
+    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred while migrating the database.");
+}
+
+app.Run();
